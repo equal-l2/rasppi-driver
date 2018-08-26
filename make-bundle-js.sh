842bd541199e3e752931df9bfd713bf882eb5e13
equal-l2 <eng.equall2@gmail.com>
2018-08-26 18:41:58 +0900

    Use howler.js for music
    Resolves the audio playback issue on WebKit

--- make-bundle-js.sh
+++ make-bundle-js.sh
@@ -4,6 +4,7 @@ OUTFILE="bundle.js"
 pushd $TMPDIR
 curl -fsSLO "https://code.jquery.com/jquery-2.2.4.min.js"
 curl -fsSLO "https://raw.githubusercontent.com/nohtcoltd/turnbox_js/master/turnBox.js"
-uglifyjs jquery-2.2.4.min.js turnBox.js -o $OUTFILE -c -m
+curl -fsSLO "https://cdnjs.cloudflare.com/ajax/libs/howler/2.0.15/howler.min.js"
+uglifyjs *.js -o $OUTFILE -c -m
 popd
 cp $TMPDIR/$OUTFILE .
