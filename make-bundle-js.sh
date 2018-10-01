f6f331f163055550d253a36a951fac70e71fe9db
equal-l2 <eng.equall2@gmail.com>
2018-10-01 19:22:11 +0900

    Use hls.js

--- make-bundle-js.sh
+++ make-bundle-js.sh
@@ -1,10 +1,12 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env sh
 TMPDIR=`mktemp -d`
 OUTFILE="bundle.js"
-pushd $TMPDIR
+CURDIR=`pwd`
+cd $TMPDIR
 curl -fsSLO "https://code.jquery.com/jquery-2.2.4.min.js"
 curl -fsSLO "https://raw.githubusercontent.com/nohtcoltd/turnbox_js/master/turnBox.js"
 curl -fsSLO "https://cdnjs.cloudflare.com/ajax/libs/howler/2.0.15/howler.min.js"
+curl -fsSL "https://cdn.jsdelivr.net/npm/hls.js@latest" -o hls.js
 uglifyjs *.js -o $OUTFILE -c -m
-popd
+cd $CURDIR
 cp $TMPDIR/$OUTFILE .
